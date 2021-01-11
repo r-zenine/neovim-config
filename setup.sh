767fdee9efeb78630cad8b615d2c2c1f17d3e081
Ryad ZENINE <r.zenine@gmail.com>
2021-01-11 12:52:34 +0100

    almost working

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ pip3 install --user --upgrade neovim-remote
 sudo apt install -y nodejs npm
 nvim -c "PlugInstall|q|q"
 
-# YCMD configuration
-# sudo apt install build-essential cmake vim-nox python3-dev nodejs
-# python3 ./plugged/YouCompleteMe/install.py --rust-completer --go-completer --clang-completer
+# Vimspector configuration:
+./plugged/vimspector/install_gadget.py --enable-python --enable-cpp --enable-rust --enable-go
