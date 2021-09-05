ba04ed722b4b0400ab726d736d284fe7b4892479
Ryad ZENINE <r.zenine@gmail.com>
2021-09-05 23:47:36 +0200

    remove highlights of symboles while cursor not moving

--- lua/configs/lsp_prelude.lua
+++ lua/configs/lsp_prelude.lua
@@ -58,18 +58,18 @@ function M.on_attach(client, bufnr)
 
 
     -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
-        hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-        hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-        hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-        augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-        ]], false)
-    end
+    -- if client.resolved_capabilities.document_highlight then
+    --     vim.api.nvim_exec([[
+    --     hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+    --     hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+    --     hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+    --     augroup lsp_document_highlight
+    --     autocmd! * <buffer>
+    --     autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+    --     autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    --     augroup END
+    --     ]], false)
+    -- end
 end
 
 
