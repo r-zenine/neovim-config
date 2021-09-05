cef71229f3a7e27e65307c3b7e43ab33b23c4e64
Ryad ZENINE <r.zenine@gmail.com>
2021-09-05 23:48:11 +0200

    Install delve in the setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 pip3 install --user --upgrade pynvim
 pip3 install --user --upgrade neovim-remote
 nvim -c "PlugInstall|q|q"
+
 # download thesaurus for vim-lexical
 curl https://raw.githubusercontent.com/zeke/moby/master/words.txt --output ./thesaurus/words.txt
 
-# Vimspector configuration:
-# ./plugged/vimspector/install_gadget.py --enable-python --enable-cpp --enable-rust --enable-go
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#    mkdir -p ./plugged/vimspector/configurations/linux/scala
-#    cp ./dap-adapters/coc-metals.json plugged/vimspector/configurations/linux/coc-metals.json
-# elif [[ "$OSTYPE" == "darwin"* ]]; then
-#    mkdir -p ./plugged/vimspector/configurations/macos/scala
-#    cp ./dap-adapters/coc-metals.json plugged/vimspector/configurations/macos/coc-metals.json
-# fi
+# enable dev mode in MacOs to prevent the prompt for passeword everytime
+sudo /usr/sbin/DevToolsSecurity -enable
+sudo dscl . append /Groups/_developer GroupMembership $(whoami)
+
+# install delve
+go install github.com/go-delve/delve/cmd/dlv@latest
+ 
+ 
