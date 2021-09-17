7eebb2d3dc525e8bc817254ae23901c64bf2f2ee
Ryad ZENINE <r.zenine@gmail.com>
2021-09-17 12:28:54 +0200

    Enable orgmode

---
+++ ftplugin/org.lua
@@ -0,0 +1,13 @@
+require'compe'.setup {
+       source = {
+           path = true;
+           buffer = true;
+           calc = true;
+           nvim_lsp = false;
+           nvim_lua = false;
+           vsnip = false;
+           ultisnips = false;
+           luasnip = false;
+           orgmode = true;
+       };
+    }
